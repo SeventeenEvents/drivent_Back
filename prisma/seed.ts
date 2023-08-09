a4fba7a93032d765d8a52907ae874a219067dc5c
Guilfornari <guilfornari@gmail.com>
2023-08-09 00:41:31 -0300

    Fixed seed not creating

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,23 +3,9 @@ import dayjs from "dayjs";
 const prisma = new PrismaClient();
 
 async function main() {
-  let event = await prisma.event.findFirst();
-  if (!event) {
-    event = await prisma.event.create({
-      data: {
-        title: "Driven.t",
-        logoImageUrl: "https://files.driveneducation.com.br/images/logo-rounded.png",
-        backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
-        startsAt: dayjs().toDate(),
-        endsAt: dayjs().add(21, "days").toDate(),
-      },
-    });
-  }
-
-  console.log({ event });
-
+  console.log("Starting...")
   let ticketTypes = await prisma.ticketType.findMany();
-  if (ticketTypes.length === 0) {
+  if (ticketTypes.length === 2) {
     const presencialTicket = await prisma.ticketType.create({
       data: {
         name: "Presencial",
@@ -86,6 +72,21 @@ async function main() {
     console.log(payment);
   }
 
+  let event = await prisma.event.findFirst();
+  if (!event) {
+    event = await prisma.event.create({
+      data: {
+        title: "Driven.t",
+        logoImageUrl: "https://files.driveneducation.com.br/images/logo-rounded.png",
+        backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
+        startsAt: dayjs().toDate(),
+        endsAt: dayjs().add(21, "days").toDate(),
+      },
+    });
+  }
+
+  console.log({ event });
+
 }
 
 main()
@@ -96,3 +97,4 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
+
